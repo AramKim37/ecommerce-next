7bfa6601ac53cf4e02ea57d9e2456c75b7481fe7
AramKim37 <aramkim37@gmail.com>
2024-06-01 20:16:53 -0600

    add menu components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,13 @@
+import Link from "next/link";
+import Menu from "./Menu";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+      {/* Mobile */}
+      <Link href="/">LAMA</Link>
+      <Menu />
+    </div>
+  );
 };
 export default Navbar;
