c33ebffc34da1f61ec45a93904772b08d59c9093
AramKim37 <aramkim37@gmail.com>
2024-06-07 17:47:00 -0600

    fixed bug

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -1,12 +1,26 @@
 "use client";
 
 import Image from "next/image";
+import Woman from "../../public/woman.png";
 
 const CartModal = () => {
   return (
-    <div className="absolute top-10 left-10 border-2">
+    <div className="absolute top-10 -left-20 border-2 p-2 w-[300px]">
       <h1>Shopping Cart</h1>
-      <div></div>
+      <div className="flex">
+        <Image src={Woman} width={64} height={64} alt="" />
+        <div className="flex">
+          <div>
+            <h1>Product Name</h1>
+            <p>Available</p>
+            <p>Qty 1</p>
+          </div>
+          <div>
+            <p>$30.5</p>
+            <p>Remove</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
