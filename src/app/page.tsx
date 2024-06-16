4ff7ba39189c1f31cb82cf4badd138cfa0126062
AramKim37 <aramkim37@gmail.com>
2024-06-15 21:28:28 -0600

    add category list component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CategoryList from "@/components/CategoryList";
 import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
 
@@ -10,11 +11,11 @@ const HomePage = () => {
         <ProductList />
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h1 className="text-2xl">Featured Product</h1>
-        <ProductList />
+        <h1 className="text-2xl">Categories</h1>
+        <CategoryList />
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h1 className="text-2xl">Featured Product</h1>
+        <h1 className="text-2xl">New Product</h1>
         <ProductList />
       </div>
     </div>
