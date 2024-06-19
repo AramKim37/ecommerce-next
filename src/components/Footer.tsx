3deecee7943b830baf357f77c44ccf65f0a6c85e
AramKim37 <aramkim37@gmail.com>
2024-06-18 21:13:18 -0600

    add icons in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,27 +1,26 @@
 import Image from "next/image";
 import Facebook from "../../public/facebook.png";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div>
       {/* top */}
-      <div>
+      <div className="flex justify-between">
         {/* left */}
-        <div className="w-1/4 mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex flex-col  gap-5">
-          <h1 className="text-2xl font-semibold">ELLE</h1>
-          <p>19 somerglen road sw</p>
-          <p>aramkim@gmail.com</p>
-          <p>+1 403 996 4709</p>
-          <div className="flex w-full gap-3">
-            <Image src={Facebook} alt="" width={20} height={20} />
-            <Image src={Facebook} alt="" width={20} height={20} />
-            <Image src={Facebook} alt="" width={20} height={20} />
-            <Image src={Facebook} alt="" width={20} height={20} />
-            <Image src={Facebook} alt="" width={20} height={20} />
-            <Image src={Facebook} alt="" width={20} height={20} />
-          </div>
-          <div className="w-full">
-            <p>@2024 Elle shop</p>
+        <div className="w-1/4 flex flex-col gap-8">
+          <Link href="/">
+            <div className="text-2xl tracking-wide">Elle</div>
+          </Link>
+          <p>19 somerglen road south west</p>
+          <span>aramkim37@gmail.com</span>
+          <span>+1 403 996 4709</span>
+          <div className="flex gap-6">
+            <Image src="/facebook.png" alt="" width={16} height={16} />
+            <Image src="/instagram.png" alt="" width={16} height={16} />
+            <Image src="/youtube.png" alt="" width={16} height={16} />
+            <Image src="/pinterest.png" alt="" width={16} height={16} />
+            <Image src="/x.png" alt="" width={16} height={16} />
           </div>
         </div>
         {/* center */}
