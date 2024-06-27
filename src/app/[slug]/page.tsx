590948064555bef748a521c567fcfe31d307fc11
AramKim37 <aramkim37@gmail.com>
2024-06-26 18:24:38 -0600

    building customizedProducts page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import Add from "@/components/Add";
+import CustomizedProducts from "@/components/CustomizedProducts";
 import { ProductImages } from "@/components/ProductImages";
 
 const SinglePage = () => {
@@ -8,7 +10,51 @@ const SinglePage = () => {
         <ProductImages />
       </div>
       {/* Text */}
-      <div className="w-full lg:w-1/2 flex flex-col gap-6">Text</div>
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        <h1 className="text-4xl font-medium">Product Name</h1>
+        <p className="text-gray-500">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. At expedita
+          vero aliquid nobis enim voluptatum eaque ducimus est inventore iste?
+          Incidunt ratione commodi ea facilis quisquam. Voluptate quas illo in!
+        </p>
+        <div className="h-[2px] bg-gray-100" />
+        <div className="flex items-center gap-4">
+          <h3 className="text-xl text-gray-500 line-through">%59</h3>
+          <h2 className="font-medium text-2xl">%49</h2>
+        </div>
+        <div className="h-[2px] bg-gray-100" />
+        <CustomizedProducts />
+        <Add />
+        <div className="h-[2px] bg-gray-100" />
+
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
+            ipsum mollitia enim hic eligendi, est fugit dicta, quasi
+            voluptatibus reprehenderit asperiores suscipit repudiandae odit
+            praesentium quia ratione iste deserunt sunt?
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
+            ipsum mollitia enim hic eligendi, est fugit dicta, quasi
+            voluptatibus reprehenderit asperiores suscipit repudiandae odit
+            praesentium quia ratione iste deserunt sunt?
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
+            ipsum mollitia enim hic eligendi, est fugit dicta, quasi
+            voluptatibus reprehenderit asperiores suscipit repudiandae odit
+            praesentium quia ratione iste deserunt sunt?
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/CustomizedProducts.tsx
+++ src/components/CustomizedProducts.tsx
@@ -1,5 +1,12 @@
 const CustomizedProducts = () => {
-  return <div>customized products</div>;
+  return (
+    <div className="flex flex-col gap-6">
+      <h4 className="font-medium">Choose a color</h4>
+      <ul className="flex items-center gap-3"></ul>
+      <h4 className="font-medium">Chosse a size</h4>
+      <ul className="flex items-center gap-3"></ul>
+    </div>
+  );
 };
 
 export default CustomizedProducts;
