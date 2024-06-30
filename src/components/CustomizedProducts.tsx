07738d2bab36aaf7b0640dad11481c2a485dde2a
AramKim37 <aramkim37@gmail.com>
2024-06-29 20:31:39 -0600

    completed choose size section

--- src/components/CustomizedProducts.tsx
+++ src/components/CustomizedProducts.tsx
@@ -12,7 +12,17 @@ const CustomizedProducts = () => {
         </li>
       </ul>
       <h4 className="font-medium">Chosse a size</h4>
-      <ul className="flex items-center gap-3"></ul>
+      <ul className="flex items-center gap-3">
+        <li className="ring-1 ring-cartColor text-cartColor rounded-md py-1 px-4 text-sm cursor-pointer">
+          Small
+        </li>
+        <li className="ring-1 ring-cartColor text-white bg-cartColor rounded-md py-1 px-4 text-sm cursor-pointer">
+          Medium
+        </li>
+        <li className="ring-1 ring-pink-200 text-white bg-pink-200 rounded-md py-1 px-4 text-sm cursor-not-allowed">
+          Large
+        </li>
+      </ul>
     </div>
   );
 };
